2f88c27b193b12de02acdd9e7250900080428b5e
Jagrata Deb <jagratadeb2004@gmail.com>
2025-05-09 20:49:31 +0530

    reverse array using swap method

---
+++ 02 Main Concepts/03 Array ADT/17ReverseArraySwap.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Array
+{
+    int A[10];
+    int size;
+    int length;
+};
+
+// Display array elements
+void display(struct Array a)
+{
+    printf("\nArray:\n");
+    for (int i = 0; i < a.length; i++)
+    {
+        printf("%d ", a.A[i]); // Print element
+    }
+}
+
+// Reverse array using swapping
+void reverse(struct Array *arr)
+{
+    for (int i = 0; i < (arr->length) / 2; i++) // Loop till middle
+    {
+        int temp = arr->A[i]; // Swap elements
+        arr->A[i] = arr->A[arr->length - 1 - i];
+        arr->A[arr->length - 1 - i] = temp;
+    }
+}
+
+int main()
+{
+    struct Array arr = {{1, 2, 3, 4, 5, 6, 7, 8, 9}, 10, 9}; // Initialize array
+
+    display(arr);  // Show original array
+    reverse(&arr); // Reverse array
+    display(arr);  // Show reversed array
+
+    return 0;
+}
