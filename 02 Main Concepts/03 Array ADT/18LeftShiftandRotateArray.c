48f01cfa70181f7403053b036255eb6c1208db67
Jagrata Deb <jagratadeb2004@gmail.com>
2025-05-11 21:33:33 +0530

    Add function prototype and comments

--- 02 Main Concepts/03 Array ADT/18LeftShiftandRotateArray.c
+++ 02 Main Concepts/03 Array ADT/18LeftShiftandRotateArray.c
@@ -1,11 +1,40 @@
 #include <stdio.h>
 
+// Define a structure to represent an array
 struct Array
 {
     int A[5];
     int size;
 };
 
+// Function prototypes
+void leftShift(struct Array *arr);
+void leftRotate(struct Array *arr);
+void display(struct Array arr);
+
+int main()
+{
+    // Initialize the array with elements and size
+    struct Array arr = {{1, 2, 3, 4, 5}, 5};
+
+    // Display the original array
+    printf("\nShow the original array:\n");
+    display(arr);
+
+    // Perform left shift operation and display the array
+    printf("Show the array after left shift:\n");
+    leftShift(&arr);
+    display(arr);
+
+    // Perform left rotate operation and display the array
+    printf("Show the array after left rotate:\n");
+    leftRotate(&arr);
+    display(arr);
+
+    return 0;
+}
+
+// Function to perform left shift operation on the array
 void leftShift(struct Array *arr)
 {
     for (int i = 0; i < arr->size - 1; i++)
@@ -15,6 +44,7 @@ void leftShift(struct Array *arr)
     arr->A[arr->size - 1] = 0;
 }
 
+// Function to perform left rotate operation on the array
 void leftRotate(struct Array *arr)
 {
     int ele = arr->A[0];
@@ -25,6 +55,7 @@ void leftRotate(struct Array *arr)
     arr->A[arr->size - 1] = ele;
 }
 
+// Function to display the elements of the array
 void display(struct Array arr)
 {
     for (int i = 0; i < arr.size; i++)
@@ -32,22 +63,4 @@ void display(struct Array arr)
         printf("%d ", arr.A[i]);
     }
     printf("\n");
-}
-
-int main()
-{
-    struct Array arr = {{1, 2, 3, 4, 5}, 5};
-
-    printf("\nShow the original array:\n");
-    display(arr);
-
-    printf("Show the array after left shift:\n");
-    leftShift(&arr);
-    display(arr);
-
-    printf("Show the array after left rotate:\n");
-    leftRotate(&arr);
-    display(arr);
-
-    return 0;
 }
