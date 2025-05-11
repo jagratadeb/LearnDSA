e721e6146dec61ced2b6ecf7ebaecd1d7b9bb58e
Jagrata Deb <jagratadeb2004@gmail.com>
2025-05-11 21:33:46 +0530

    Add function prototype and comments

--- 02 Main Concepts/03 Array ADT/19RightShiftandRotate.c
+++ 02 Main Concepts/03 Array ADT/19RightShiftandRotate.c
@@ -1,11 +1,40 @@
 #include <stdio.h>
 
+// Define a structure to represent an array
 struct Array
 {
     int A[5];
     int size;
 };
 
+// Function prototypes
+void rightShift(struct Array *arr);
+void rightRotate(struct Array *arr);
+void display(struct Array arr);
+
+int main()
+{
+    // Initialize the array with elements and size
+    struct Array arr = {{1, 2, 3, 4, 5}, 5};
+
+    // Display the original array
+    printf("\nShow the original array:\n");
+    display(arr);
+
+    // Perform right shift operation and display the array
+    printf("Show the array after right shift:\n");
+    rightShift(&arr);
+    display(arr);
+
+    // Perform right rotate operation and display the array
+    printf("Show the array after right rotate:\n");
+    rightRotate(&arr);
+    display(arr);
+
+    return 0;
+}
+
+// Function to perform right shift operation on the array
 void rightShift(struct Array *arr)
 {
     for (int i = arr->size - 1; i >= 0; i--)
@@ -15,9 +44,10 @@ void rightShift(struct Array *arr)
     arr->A[0] = 0;
 }
 
+// Function to perform right rotate operation on the array
 void rightRotate(struct Array *arr)
 {
-    int ele = arr->A[arr->size-1];
+    int ele = arr->A[arr->size - 1];
     for (int i = arr->size - 1; i >= 0; i--)
     {
         arr->A[i] = arr->A[i - 1];
@@ -25,6 +55,7 @@ void rightRotate(struct Array *arr)
     arr->A[0] = ele;
 }
 
+// Function to display the elements of the array
 void display(struct Array arr)
 {
     for (int i = 0; i < arr.size; i++)
@@ -32,22 +63,4 @@ void display(struct Array arr)
         printf("%d ", arr.A[i]);
     }
     printf("\n");
-}
-
-int main()
-{
-    struct Array arr = {{1, 2, 3, 4, 5}, 5};
-
-    printf("\nShow the original array:\n");
-    display(arr);
-
-    printf("Show the array after right shift:\n");
-    rightShift(&arr);
-    display(arr);
-
-    printf("Show the array after right rotate:\n");
-    rightRotate(&arr);
-    display(arr);
-
-    return 0;
 }
