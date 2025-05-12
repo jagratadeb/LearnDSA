b34328bdd1c83d21111005d836b03c39d1be8b9c
Jagrata Deb <jagratadeb2004@gmail.com>
2025-05-12 21:58:47 +0530

    Fix minor mistake

--- 02 Main Concepts/03 Array ADT/23MergeArray.c
+++ 02 Main Concepts/03 Array ADT/23MergeArray.c
@@ -58,11 +58,11 @@ struct Array *merge(struct Array *arr1, struct Array *arr2)
     }
     for (; i < arr1->length; i++)
     {
-        arr3->A[k++] = arr1->A[i++];
+        arr3->A[k++] = arr1->A[i];
     }
     for (; j < arr2->length; j++)
     {
-        arr3->A[k++] = arr2->A[j++];
+        arr3->A[k++] = arr2->A[j];
     }
     arr3->length = arr1->length + arr2->length;
     arr3->size = 10;
