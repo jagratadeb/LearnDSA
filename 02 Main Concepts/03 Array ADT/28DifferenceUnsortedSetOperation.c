7010aa0684a70e43ca09ecd62ca4eeb9d53fa7c8
Jagrata Deb <jagratadeb2004@gmail.com>
2025-05-13 23:26:54 +0530

    Add C codes for finding difference between two sorted or unsorted arrays

---
+++ 02 Main Concepts/03 Array ADT/28DifferenceUnsortedSetOperation.c
@@ -0,0 +1,70 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Array
+{
+    int A[10];
+    int size;
+    int length;
+};
+
+// Function prototype
+void display(struct Array arr);
+struct Array *differenceUnsorted(struct Array *arr1, struct Array *arr2);
+
+int main()
+{
+    struct Array arr1 = {{3, 5, 10, 4, 6}, 10, 5};
+    struct Array arr2 = {{12, 4, 7, 2, 5}, 10, 5};
+    struct Array *arr3;
+
+    printf("\nArray 1:\n");
+    display(arr1);
+    printf("\nArray 2:\n");
+    display(arr2);
+
+    arr3 = differenceUnsorted(&arr1, &arr2);
+
+    printf("\nUnsorted difference Array:\n");
+    display(*arr3);
+
+    free(arr3);
+    return 0;
+}
+
+void display(struct Array arr)
+{
+    for (int i = 0; i < arr.length; i++)
+    {
+        printf("%d ", arr.A[i]);
+    }
+    printf("\n");
+}
+
+struct Array *differenceUnsorted(struct Array *arr1, struct Array *arr2)
+{
+    int k = 0;
+    struct Array *arr3 = (struct Array *)malloc(sizeof(struct Array));
+    arr3->length = 0;
+    arr3->size = arr1->size + arr2->size;
+
+    for (int i = 0; i < arr1->length; i++)
+    {
+        int isDuplicate = 0;
+        for (int j = 0; j < arr2->length; j++)
+        {
+            if (arr1->A[i] == arr2->A[j])
+            {
+                isDuplicate = 1;
+                break;
+            }
+        }
+        if (!isDuplicate)
+        {
+            arr3->A[k++] = arr1->A[i];
+            arr3->length++;
+        }
+    }
+
+    return arr3;
+}
---
+++ 02 Main Concepts/03 Array ADT/29DifferenceSortedSetOperation.c
@@ -0,0 +1,75 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Array
+{
+    int A[10];
+    int size;
+    int length;
+};
+
+// Function prototype
+void display(struct Array arr);
+struct Array *differenceSorted(struct Array *arr1, struct Array *arr2);
+
+int main()
+{
+    struct Array arr1 = {{3, 4, 5, 6, 10}, 10, 5};
+    struct Array arr2 = {{2, 4, 5, 7, 12}, 10, 5};
+    struct Array *arr3;
+    arr3 = differenceSorted(&arr1, &arr2);
+
+    printf("Display Array 1:\n");
+    display(arr1);
+    printf("Display Array 2:\n");
+    display(arr2);
+    printf("Sorted difference Array:\n");
+    display(*arr3);
+
+    free(arr3);
+    return 0;
+}
+
+void display(struct Array arr)
+{
+    for (int i = 0; i < arr.length; i++)
+    {
+        printf("%d ", arr.A[i]); // Print each element
+    }
+    printf("\n"); // Print a newline after displaying all elements
+}
+
+struct Array *differenceSorted(struct Array *arr1, struct Array *arr2)
+{
+    int i = 0;
+    int j = 0;
+    int k = 0;
+    struct Array *arr3;
+    arr3 = (struct Array *)malloc(sizeof(struct Array));
+
+    arr3->length = 0;
+    while (i < arr1->length && j < arr2->length)
+    {
+        if (arr1->A[i] < arr2->A[j])
+        {
+            arr3->A[k++] = arr1->A[i++];
+        }
+        else if (arr1->A[i] > arr2->A[j])
+        {
+            j++;
+        }
+        else
+        {
+            i++;
+            j++;
+        }
+    }
+    for (; i < arr1->length; i++)
+    {
+        arr3->A[k++] = arr1->A[i];
+    }
+    arr3->length = k;
+    arr3->size = arr1->size + arr2->size;
+
+    return arr3;
+}
