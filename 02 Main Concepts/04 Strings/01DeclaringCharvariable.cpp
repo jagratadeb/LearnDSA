0cc796c0c75690b5b454949ef37e4d44894aec4e
Jagrata Deb <jagratadeb2004@gmail.com>
2025-05-17 22:09:18 +0530

    Rewrite C code in C++

---
+++ 02 Main Concepts/04 Strings/01DeclaringCharvariable.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    char temp = 'A';
+    cout << "Character = " << temp << endl;
+    cout << "ASCII = " << (int)temp << endl;
+    return 0;
+}
