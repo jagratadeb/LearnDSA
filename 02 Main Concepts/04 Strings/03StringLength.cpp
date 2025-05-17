83ef31196464e1b80bf51b759189d20cc4039aa9
Jagrata Deb <jagratadeb2004@gmail.com>
2025-05-17 17:49:32 +0530

    length of a string

---
+++ 02 Main Concepts/04 Strings/03StringLength.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    char name[] = "Jagrata Deb";
+    int i;
+    for (i = 0; name[i] != '\0'; i++)
+    {
+    }
+    printf("Length of \"%s\" is %d ", name, i);
+    return 0;
+}
