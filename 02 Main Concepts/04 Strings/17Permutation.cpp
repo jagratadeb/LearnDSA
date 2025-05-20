6e4116f2e6db01090f5dae9d9297d356754bbcf7
Jagrata Deb <jagratadeb2004@gmail.com>
2025-05-20 21:22:00 +0530

    Find out permutations

---
+++ 02 Main Concepts/04 Strings/17Permutation.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+void permutation(char s[], int k)
+{
+    static int A[10] = {0};
+    static char Res[10];
+    int i;
+    if (s[k] == '\0')
+    {
+        Res[k] = '\0';
+        cout << Res << endl;
+    }
+    else
+    {
+        for (i = 0; s[i] != '\0'; i++)
+        {
+            if (A[i] == 0)
+            {
+                Res[k] = s[i];
+                A[i] = 1;
+                permutation(s, k + 1);
+                A[i] = 0;
+            }
+        }
+    }
+}
+
+int main()
+{
+    char string[] = "ABC";
+    permutation(string, 0);
+    return 0;
+}
---
+++ 02 Main Concepts/04 Strings/18PermutaionSwap.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string.h>
+using namespace std;
+
+void permutation(char s[], int l, int h)
+{
+    int i;
+    if (l == h)
+    {
+        cout << s << " ";
+    }
+    else
+    {
+        for (int i = 0; i <= h; i++)
+        {
+            swap(s[l], s[i]);
+            permutation(s, l + 1, h);
+            swap(s[l], s[i]);
+        }
+    }
+}
+
+int main()
+{
+    char string[] = "ABC";
+    int lastIndex = strlen(string) - 1;
+    permutation(string, 0, lastIndex);
+
+    return 0;
+}
