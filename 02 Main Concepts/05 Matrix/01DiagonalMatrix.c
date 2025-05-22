04b040579168355525af1b1cdf5320f4c0ce02e4
Jagrata Deb <jagratadeb2004@gmail.com>
2025-05-22 14:08:30 +0530

    Diagonal Matrix

---
+++ 02 Main Concepts/05 Matrix/01DiagonalMatrix.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+
+struct Matrix
+{
+    int A[10];
+    int dimension;
+};
+
+void set(struct Matrix *m, int i, int j, int ele)
+{
+    if (i == j)
+    {
+        m->A[i] = ele; // Corrected indexing
+    }
+}
+
+int get(struct Matrix m, int i, int j)
+{
+    if (i == j)
+        return m.A[i]; // Corrected indexing
+    return 0;
+}
+
+void display(struct Matrix m)
+{
+    int i, j;
+    for (i = 0; i < m.dimension; i++)
+    {
+        for (j = 0; j < m.dimension; j++)
+        {
+            if (i == j)
+                printf("%d ", m.A[i]); // Corrected indexing
+            else
+                printf("0 ");
+        }
+        printf("\n");
+    }
+}
+
+int main()
+{
+    struct Matrix m;
+    m.dimension = 4;
+
+    set(&m, 0, 0, 5);
+    set(&m, 1, 1, 10);
+    set(&m, 2, 2, 15);
+    set(&m, 3, 3, 20);
+
+    printf("Element at (4,4): %d\n", get(m, 3, 3));
+    printf("Element at (2,2): %d\n", get(m, 1, 1));
+    printf("Element at (1,2): %d\n", get(m, 0, 1));
+    printf("Element at (2,1): %d\n", get(m, 1, 0));
+
+    display(m);
+    return 0;
+}
