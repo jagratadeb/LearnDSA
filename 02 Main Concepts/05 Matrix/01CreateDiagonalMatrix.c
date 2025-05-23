cb900a06534e4fb98e69e27bc0cca4a0adf8e808
Jagrata Deb <jagratadeb2004@gmail.com>
2025-05-23 19:43:26 +0530

    Minor fixes

--- 02 Main Concepts/05 Matrix/01CreateDiagonalMatrix.c
+++ 02 Main Concepts/05 Matrix/01CreateDiagonalMatrix.c
@@ -10,26 +10,26 @@ void set(struct Matrix *m, int i, int j, int ele)
 {
     if (i == j)
     {
-        m->A[i] = ele;
+        m->A[i - 1] = ele;
     }
 }
 
 int get(struct Matrix m, int i, int j)
 {
     if (i == j)
-        return m.A[i];
+        return m.A[i - 1];
     return 0;
 }
 
 void display(struct Matrix m)
 {
     int i, j;
-    for (i = 0; i < m.dimension; i++)
+    for (i = 1; i <= m.dimension; i++)
     {
-        for (j = 0; j < m.dimension; j++)
+        for (j = 1; j <= m.dimension; j++)
         {
             if (i == j)
-                printf("%d ", m.A[i]);
+                printf("%d ", m.A[i - 1]);
             else
                 printf("0 ");
         }
@@ -42,16 +42,16 @@ int main()
     struct Matrix m;
     m.dimension = 5;
 
-    set(&m, 0, 0, 5);
-    set(&m, 1, 1, 10);
-    set(&m, 2, 2, 15);
-    set(&m, 3, 3, 20);
-    set(&m, 4, 4, 25);
+    set(&m, 1, 1, 5);
+    set(&m, 2, 2, 10);
+    set(&m, 3, 3, 15);
+    set(&m, 4, 4, 20);
+    set(&m, 5, 5, 25);
 
-    printf("Element at (4,4): %d\n", get(m, 3, 3));
-    printf("Element at (2,2): %d\n", get(m, 1, 1));
-    printf("Element at (1,2): %d\n", get(m, 0, 1));
-    printf("Element at (2,1): %d\n", get(m, 1, 0));
+    printf("Element at (4,4): %d\n", get(m, 4, 4));
+    printf("Element at (2,2): %d\n", get(m, 2, 2));
+    printf("Element at (1,2): %d\n", get(m, 1, 2));
+    printf("Element at (2,1): %d\n", get(m, 2, 1));
 
     display(m);
     return 0;
