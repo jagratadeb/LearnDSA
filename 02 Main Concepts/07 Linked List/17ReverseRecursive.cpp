7e7b2642962d292771c345d41da6515d5ef499e9
Jagrata Deb <jagratadeb2004@gmail.com>
2025-06-10 20:31:07 +0530

    add code for reversing a linked list using recursion

---
+++ 02 Main Concepts/07 Linked List/17ReverseRecursive.cpp
@@ -0,0 +1,80 @@
+#include <iostream>
+using namespace std;
+
+struct Node
+{
+    int data;
+    struct Node *next;
+} *first;
+
+void display(struct Node *first);
+void create(int arr[], int size);
+void createArray(int *&arr, int &n);
+void reverse(struct Node *q, struct Node *p);
+
+int main()
+{
+    int *arr;
+    int n;
+    createArray(arr, n);
+    create(arr, n);
+    display(first);
+    reverse(NULL, first);
+    display(first);
+    return 0;
+}
+
+void createArray(int *&arr, int &n)
+{
+    cout << "Enter number of elements:\n";
+    cin >> n;
+    arr = new int[n];
+    cout << "Enter the elements:\n";
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+}
+
+void create(int arr[], int size)
+{
+    struct Node *last, *t;
+    first = new Node();
+    first->data = arr[0];
+    first->next = NULL;
+    last = first;
+
+    for (int i = 1; i < size; i++)
+    {
+        t = new Node();
+        t->data = arr[i];
+        t->next = NULL;
+        last->next = t;
+        last = t;
+    }
+}
+
+void display(struct Node *first)
+{
+    struct Node *t;
+    t = first;
+    cout << "\nDisplaying the Linked List:" << endl;
+    while (t != NULL)
+    {
+        cout << t->data << " ";
+        t = t->next;
+    }
+}
+
+void reverse(struct Node *q, struct Node *p)
+{
+    if (p != NULL)
+    {
+        reverse(p, p->next);
+        p->next = q;
+    }
+    else
+    {
+        first = q;
+    }
+}
