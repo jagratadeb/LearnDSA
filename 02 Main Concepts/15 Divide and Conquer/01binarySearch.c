3c22407b9db5c0b14bf0a1d183afe852863fdf78
Jagrata Deb <jagratadeb2004@gmail.com>
2025-08-25 02:21:34 +0530

    add code for DAC Binary Search

---
+++ 02 Main Concepts/15 Divide and Conquer/01binarySearch.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int DACRecursiveBinarySearch(int *arr, int low, int high, int key)
+{
+    int mid = (low + high) / 2;
+    if (low > high)
+        return -1;
+
+    if (key == arr[mid])
+    {
+        // These commented line can return the first occurence of key element.
+        // int leftIndex = DACRecursiveBinarySearch(arr, low, mid - 1, key);
+        // return (leftIndex != -1) ? leftIndex : mid;
+        return mid;
+    }
+
+    else if (key < arr[mid])
+        DACRecursiveBinarySearch(arr, low, mid - 1, key);
+    else
+        DACRecursiveBinarySearch(arr, mid + 1, high, key);
+}
+
+int main()
+{
+    int arr[] = {-22, -16, -10, -6, 1, 6, 10, 15, 20, 22, 50, 70, 75, 80, 81};
+    int size = sizeof(arr) / sizeof(int);
+    int key;
+    scanf("%d", &key);
+    printf("The index of %d is %d\n", key, DACRecursiveBinarySearch(arr, 0, size - 1, key));
+
+    return 0;
+}
