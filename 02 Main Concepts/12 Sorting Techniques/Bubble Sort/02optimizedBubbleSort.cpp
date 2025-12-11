c37fcc3d9cbce88925553979c9c79c16f3ae739a
Jagrata Deb <jagratadeb2004@gmail.com>
2025-12-12 00:35:57 +0530

    Fix code for Bubble Sort

--- 02 Main Concepts/12 Sorting Techniques/Bubble Sort/02optimizedBubbleSort.cpp
+++ 02 Main Concepts/12 Sorting Techniques/Bubble Sort/02optimizedBubbleSort.cpp
@@ -14,29 +14,28 @@ void display(int arr[], int size)
 void bubbleSort(int arr[], int size)
 {
     cout << "\nBubble Sort .....\n";
-    int flag = 0;
+
     for (int i = 0; i < size - 1; i++)
     {
-        flag = 0;
+        bool sorted = true;
+
         for (int j = 0; j < size - 1 - i; j++)
         {
             if (arr[j] > arr[j + 1])
             {
                 swap(arr[j], arr[j + 1]);
-                flag = 1;
+                sorted = false;
             }
         }
-        if (flag == 0)
-        {
-            cout << "\nArray is already sorted." << endl;
+
+        if (sorted)
             break;
-        }
     }
 }
 
 int main()
 {
-    int arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    int arr[] = {1, 2, 3, 4, 10, 9, 8, 7, 6, 5};
     int size = sizeof(arr) / sizeof(int);
 
     display(arr, size);
